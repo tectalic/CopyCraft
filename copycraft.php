afc1ce097a60c8d4355d4d1fa3f556b2fdb50998
James Collins <james@om4.com.au>
2023-01-03 14:46:19 +0800

    Plugin scaffold
    wp plugin scaffold copycraft --plugin_name=CopyCraft --plugin_author="OM4 Software"
    
    
    Co-authored-by: Csaba Maulis <csaba@om4.com.au>

---
+++ copycraft.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     CopyCraft
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          OM4 Software
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     copycraft
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Copycraft
+ */
+
+// Your code starts here.
