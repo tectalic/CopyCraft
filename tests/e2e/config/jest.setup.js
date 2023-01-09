d5933adafae784ba974174d2e6da58bfae679e0f
Csaba Maulis <csaba@om4.com.au>
2023-01-09 17:38:13 +0800

    Tweak node/npm environment for e2e tests
    Remove Grunt - not planning to use
    
    Reconfigure testing environment:
    -  Remove node version constraint and dependency on nvm.
    - Use @wordpress/e2e-test-utils instead of @woocommerce/e2e-utils because better supported.
    - Use @wordpress/env instead of @woocommerce/e2e-environment because better supported.
    - Remove other @woocommerce/api. Prefer using Puppeteer API directly and jest-puppeteer for config.
    
    Code style:
    Add the Prettier package and config too.
    
    Docker environment
    - Managed by wp-env (@wordpress/env)
    - Run custom initialise script on both environments (dev and test)
    - Handle plugin installs via wp-env config 
    
    Misc:
    - make test-related files ignored from plugin distribution.
    - Update Test Readme file

--- tests/e2e/config/jest.setup.js
+++
@@ -1,8 +0,0 @@
-function setTestTimeouts() {
-	// Increase the default test execution timeout from 5 seconds to 20 seconds.
-	jest.setTimeout( 20000 );
-}
-
-beforeEach( async () => {
-	setTestTimeouts();
-});
