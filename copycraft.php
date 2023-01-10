tectalic/CopyCraft

commit 1bd73d79dc3fa589c795a21d1c9345128bf8b94f

Initialise the plugin during admin_init (during admin page loads), and never during frontend page loads

copycraft.php
@@ -66,16 +66,17 @@ class Plugin {
 		);
 
 		$this->container->delegate( new ReflectionContainer( true ) );
-		add_action( 'init', array( $this, 'init' ) );
+
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
 	}
 
 	/**
 	 * Initialise the plugin, including all WordPress hooks/filters/actions.
-	 * Executed during the `init` hook.
+	 * Executed during the `admin_init` hook.
 	 *
 	 * @return void
 	 */
-	public function init() {
+	public function admin_init() {
 		/**
 		 * Register instance for settings screen.
 		 *
@@ -90,7 +91,7 @@ class Plugin {
 		 * @var ModalRegister $modal
 		 */
 		$modal = $this->container->get( ModalRegister::class );
-		add_action( 'admin_init', array( $modal, 'register_modal' ) );
+		$modal->register_modal();
 	}
 }
 

commit d1a86ad220f50775714f5b25b4c616a7e417aefd

Fix issue with missing Settings screen (bug introduced in 1bd73d79dc3fa589c795a21d1c9345128bf8b94f)
As per https://codex.wordpress.org/Plugin_API/Action_Reference, `admin_init` runs after `admin_menu`, which caused the settings menu item to disappear.

copycraft.php
@@ -67,16 +67,21 @@ class Plugin {
 
 		$this->container->delegate( new ReflectionContainer( true ) );
 
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'init', array( $this, 'admin_init' ) );
 	}
 
 	/**
 	 * Initialise the plugin, including all WordPress hooks/filters/actions.
-	 * Executed during the `admin_init` hook.
+	 * Executed during the `init` hook.
 	 *
 	 * @return void
 	 */
 	public function admin_init() {
+		if ( ! is_admin()) {
+			// Non wp-admin page.
+			return;
+		}
+
 		/**
 		 * Register instance for settings screen.
 		 *